43575651da6f213c170b151f069f0331b12f201f
Creeland <cree@provinsal.com>
2017-05-10 13:16:36 -0700

    Update Angular to 4.1.1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,8 @@
 import {Component} from "@angular/core";
 @Component({
-    selector: 'app',
+    selector:'app',
     template: `
-<nav>
-<a routerLink="">All</a>
-<a routerLink="active">Active</a>
-<a routerLink="completed">Completed</a>
-</nav>
-<router-outlet></router-outlet>
+<home></home>
 `
 })
-export class AppComponent {
-}
+export class AppComponent{}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,11 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {NgModule} from "@angular/core";
-import {routes} from "./app.routes";
-import TodoModule from "./shared/todo.module";
+import {HomeModule} from "./home/home.module";
+import {ServicesModule} from "./services/services.module";
 @NgModule({
-    imports: [BrowserModule, routes, TodoModule.forRoot()],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    imports:[BrowserModule, HomeModule, ServicesModule.forRoot()],
+    declarations:[AppComponent],
+    bootstrap:[AppComponent]
 })
-export class AppModule {
-}
+export class AppModule{}
