ee04910fad79f430973254456120feef751ee451
Zac Jones <zacjones93@gmail.com>
2017-06-16 12:35:41 -0700

    updated angular to 4.2.2

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,13 @@
+export class AppModule { }
 import {BrowserModule} from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from "./app.component";
 import {NgModule} from "@angular/core";
 
 @NgModule({
-    imports:[BrowserModule],
+    imports:[
+      BrowserModule,
+      BrowserAnimationsModule],
     declarations:[AppComponent],
     bootstrap:[AppComponent]
 })
